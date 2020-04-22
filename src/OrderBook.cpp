11da22e7b6f91566e4e70f3322d9eb9edf1ed866
Brandon Liang <54skyxenon@gmail.com>
2020-04-22 03:15:40 -0700

    Final touches, for now...

--- src/OrderBook.cpp
+++ src/OrderBook.cpp
@@ -4,19 +4,14 @@
 
 #include "OrderBook.h"
 
-// do we have anything in the OrderBook?
-bool OrderBook::is_empty() const {
-    return bids.empty() && asks.empty();
-}
-
 // adds an ask to the OrderBook
 void OrderBook::add_ask(int price, int amount) {
-    add(price, amount, true);
+    add(price, amount, false);
 }
 
 // adds a bid to the OrderBook
 void OrderBook::add_bid(int price, int amount) {
-    add(price, amount, false);
+    add(price, amount, true);
 }
 
 // adds something to the OrderBook
@@ -29,6 +24,7 @@ void OrderBook::add(int price, int amount, bool bid) {
     }
 }
 
+// make toString() for an OrderBook
 std::ostream& operator<<(std::ostream& os, const OrderBook& book) {
     if (book.is_empty()) {
         os << "ORDER BOOK IS EMPTY";
@@ -47,6 +43,26 @@ std::ostream& operator<<(std::ostream& os, const OrderBook& book) {
     return os;
 }
 
+// toString() for a BidAsk object
+std::ostream &operator<<(std::ostream &os, const OrderBook::BidAsk &ba) {
+    // print will be a lambda
+    // [&] grabs by reference
+    auto print = [&](const OrderBook::BidAsk::Entry& e, const std::string& text) {
+        bool have_value = e.has_value();
+        if (have_value) {
+            auto value = e.value();
+            os << value.second << text << "s @" << value.first;
+        } else {
+            os << "NO " << text;
+        }
+    };
+
+    print(ba.bid, "bid");
+    os << ", ";
+    print(ba.ask, "ask");
+    return os;
+}
+
 // return a composite BidAsk object
 OrderBook::BidAsk OrderBook::get_bid_ask() const {
     BidAsk result;
@@ -59,6 +75,40 @@ OrderBook::BidAsk OrderBook::get_bid_ask() const {
     auto best_ask = asks.rbegin();
     if (best_ask != asks.rend()) {
         result.ask = *best_ask;
+    }
+
+    return result;
+}
+
+// removes a bid from the OrderBook
+void OrderBook::remove_bid(int price, int amount) {
+    remove(price, amount, true);
+}
+
+// removes an ask from the OrderBook
+void OrderBook::remove_ask(int price, int amount) {
+    remove(price, amount, false);
+}
+
+// removes something from the OrderBook
+void OrderBook::remove(int price, int amount, bool bid) {
+    auto& table = bid ? bids : asks;
+    auto it = table.find(price);
+
+    if (it != table.end()) {
+        it->second -= amount;
+        if (it->second == 0) {
+            table.erase(it);
+        }
+    }
+}
+
+// get the spread of the OrderBook
+std::optional<int> OrderBook::BidAsk::spread() const {
+    std::optional<int> result;
+
+    if (bid.has_value() && ask.has_value()) {
+        result = ask.value().first - bid.value().first;
     }
 
     return result;
--- src/OrderBook.h
+++ src/OrderBook.h
@@ -9,6 +9,7 @@ class OrderBook {
     // private by default
     std::map<int, int> bids, asks;
     void add(int price, int amount, bool bid);
+    void remove(int price, int amount, bool bid);
 public:
     // inner struct we use to modularize data in this OrderBook
     struct BidAsk {
@@ -17,14 +18,22 @@ public:
         std::optional<int> spread() const;
     };
 
+    // do we have anything in the OrderBook?
+    // inline tells compiler to omit multiple definitions
     // const prevents functions from mutation in C++
-    bool is_empty() const;
+    inline bool is_empty() const {
+        return bids.empty() && asks.empty();
+    }
+
     void add_bid(int price, int amount);
     void add_ask(int price, int amount);
+    void remove_bid(int price, int amount);
+    void remove_ask(int price, int amount);
 
     BidAsk get_bid_ask() const;
 
     // this is essentially toString()
     // friend makes it so it can be globally called
     friend std::ostream& operator<<(std::ostream& os, const OrderBook& book);
+    friend std::ostream& operator<<(std::ostream& os, const OrderBook::BidAsk& ba);
 };

94e4d5f7470ff3a72e52b9bbab5ea2248c28583b
Brandon Liang <54skyxenon@gmail.com>
2020-04-22 03:15:30 -0700

    Finished tests

--- test/tests/SomeTests.cpp
+++ test/tests/SomeTests.cpp
@@ -21,3 +21,37 @@ TEST(test, OrderAdditionWorks) {
     EXPECT_EQ(123, bid.first);
     EXPECT_EQ(456, bid.second);
 }
+
+TEST(test, OrderRemovalWorks) {
+    OrderBook book;
+    book.add_bid(123, 456);
+    book.add_ask(70, 340);
+
+    book.remove_bid(123, 156);
+    book.remove_ask(70, 140);
+
+    auto bid_ask = book.get_bid_ask();
+    EXPECT_TRUE(bid_ask.bid.has_value());
+    EXPECT_TRUE(bid_ask.ask.has_value());
+
+    auto bid = bid_ask.bid.value();
+    EXPECT_EQ(123, bid.first);
+    EXPECT_EQ(300, bid.second);
+
+    auto ask = bid_ask.ask.value();
+    EXPECT_EQ(70, ask.first);
+    EXPECT_EQ(200, ask.second);
+}
+
+TEST(test, SpreadCalculated) {
+    OrderBook book;
+    book.add_bid(50, 100);
+    book.add_ask(70, 100);
+
+    auto bid_ask = book.get_bid_ask();
+    EXPECT_TRUE(bid_ask.bid.has_value());
+    EXPECT_TRUE(bid_ask.ask.has_value());
+
+    auto spread = bid_ask.spread();
+    EXPECT_EQ(20, spread.value());
+}

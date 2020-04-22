0a35a5b36a0f4aec67d2844991e5600dc264afd0
Brandon Liang <54skyxenon@gmail.com>
2020-04-22 01:15:46 -0700

    Added bid ask type in orderbook class

--- src/OrderBook.cpp
+++ src/OrderBook.cpp
@@ -4,14 +4,62 @@
 
 #include "OrderBook.h"
 
+// do we have anything in the OrderBook?
 bool OrderBook::is_empty() const {
     return bids.empty() && asks.empty();
 }
 
+// adds an ask to the OrderBook
 void OrderBook::add_ask(int price, int amount) {
-
+    add(price, amount, true);
 }
 
+// adds a bid to the OrderBook
 void OrderBook::add_bid(int price, int amount) {
+    add(price, amount, false);
+}
 
+// adds something to the OrderBook
+void OrderBook::add(int price, int amount, bool bid) {
+    if (bid) {
+        bids[price] += amount;
+    }
+    else {
+        asks[price] += amount;
+    }
+}
+
+std::ostream& operator<<(std::ostream& os, const OrderBook& book) {
+    if (book.is_empty()) {
+        os << "ORDER BOOK IS EMPTY";
+        return os;
+    }
+
+    for (auto it = book.asks.rbegin(); it != book.asks.rend(); ++it) {
+        os << it->first << "\t" << it->second << std::endl;
+    }
+
+    os << std::endl;
+    for (auto it = book.bids.rbegin(); it != book.bids.rend(); ++it) {
+        os << it->first << "\t" << it->second << std::endl;
+    }
+
+    return os;
+}
+
+// return a composite BidAsk object
+OrderBook::BidAsk OrderBook::get_bid_ask() const {
+    BidAsk result;
+
+    auto best_bid = bids.rbegin();
+    if (best_bid != bids.rend()) {
+        result.bid = *best_bid;
+    }
+
+    auto best_ask = asks.rbegin();
+    if (best_ask != asks.rend()) {
+        result.ask = *best_ask;
+    }
+
+    return result;
 }
--- src/OrderBook.h
+++ src/OrderBook.h
@@ -1,15 +1,30 @@
-//
-// Created by Brandon Liang on 4/21/20.
-//
-
 #pragma once
 
 #include <map>
+#include <iostream>
+#include <optional>
 
+// An object representing an order book and its associated operations
 class OrderBook {
+    // private by default
     std::map<int, int> bids, asks;
+    void add(int price, int amount, bool bid);
 public:
+    // inner struct we use to modularize data in this OrderBook
+    struct BidAsk {
+        typedef std::optional<std::pair<int, int>> Entry;
+        Entry bid, ask;
+        std::optional<int> spread() const;
+    };
+
+    // const prevents functions from mutation in C++
     bool is_empty() const;
     void add_bid(int price, int amount);
     void add_ask(int price, int amount);
+
+    BidAsk get_bid_ask() const;
+
+    // this is essentially toString()
+    // friend makes it so it can be globally called
+    friend std::ostream& operator<<(std::ostream& os, const OrderBook& book);
 };

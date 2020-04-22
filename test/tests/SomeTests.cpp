25c18258ac1a6161b0c07b4820b6c32fe6162111
Brandon Liang <54skyxenon@gmail.com>
2020-04-22 01:16:04 -0700

    These tests don't work :(

--- test/tests/SomeTests.cpp
+++ test/tests/SomeTests.cpp
@@ -9,3 +9,15 @@ TEST(test, OrderBookEmptyByDefault) {
     OrderBook book;
     EXPECT_TRUE(book.is_empty());
 }
+
+TEST(test, OrderAdditionWorks) {
+    OrderBook book;
+    book.add_bid(123, 456);
+
+    auto bid_ask = book.get_bid_ask();
+    EXPECT_TRUE(bid_ask.bid.has_value());
+
+    auto bid = bid_ask.bid.value();
+    EXPECT_EQ(123, bid.first);
+    EXPECT_EQ(456, bid.second);
+}
